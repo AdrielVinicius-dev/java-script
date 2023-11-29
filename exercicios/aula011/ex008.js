dcab2743fc2f7c7f6b97c5f70703837769544ea4
Adriel Vinicius Werlich Cardoso <145874881+AdrielVinicius-dev@users.noreply.github.com>
2023-11-29 20:18:22 -0300

    Initial commit

---
+++ exercicios/aula011/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é de ${vel}`)
+
+if(vel > 60){
+    console.log('Voçê ultrapassou a velocidade permitida. MULTADO')
+}
+
+console.log('Dirija sempre usando cinto de segurança')

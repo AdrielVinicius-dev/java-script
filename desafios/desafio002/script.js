631d86b50ca4cb75b44c0e2ba4e8f296656c41c6
Adriel Vinicius Werlich Cardoso <145874881+AdrielVinicius-dev@users.noreply.github.com>
2023-11-29 22:26:02 -0300

    desafio002

--- desafios/desafio002/script.js
+++ desafios/desafio002/script.js
@@ -1,1 +1,18 @@
-alert('Ola')
+function carregar(){
+    var msg = document.getElementById('div1')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 6 && hora < 12) {
+        img.src = 'imagem/pexels-james-wheeler-1486974.jpg'
+        document.body.style.background = '#FF92AF'
+    } else if (hora >= 12 && hora <= 18) {
+        img.src = 'imagem/tarde.jpg'
+        document.body.style.background = '#FFA521'
+    } else {
+        img.src = 'imagem/noite.jpg'
+        document.body.style.background = '#332A42'
+    }
+    
+}

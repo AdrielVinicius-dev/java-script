0bd63402d16903ae12ffccf2c9a3178740c9e193
Adriel Vinicius Werlich Cardoso <145874881+AdrielVinicius-dev@users.noreply.github.com>
2024-01-07 20:53:34 -0300

    ex 03

--- desafios/desafio003/script.js
+++ desafios/desafio003/script.js
@@ -10,11 +10,35 @@ function verificar(){
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada ${idade}`
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                img.setAttribute('src', 'menino-criança.jpg')
+             } else if (idade < 21){
+                img.setAttribute('src', 'homem-jovem.jpg')
+             } else if (idade < 50){
+                img.setAttribute('src', 'homem-adulto.jpg')
+             } else {
+                 img.setAttribute('src', 'idoso.jpg')
+             }
+            
         } else if(fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                img.setAttribute('src', 'memina-criança.jpg')
+             } else if (idade < 21){
+                img.setAttribute('src', 'mulher-jovem.jpg')
+             } else if (idade < 50){
+                img.setAttribute('src', 'mulher-adulta.jpg')
+             } else {
+                 img.setAttribute('src', 'idosa.jpg')
+             }
+            
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }

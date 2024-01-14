411afd8b8a32488dd72faa7b0b9004102399e675
Adriel Vinicius Werlich Cardoso <145874881+AdrielVinicius-dev@users.noreply.github.com>
2024-01-14 11:34:42 -0300

    .

--- desafios/desafio002/script.js
+++ desafios/desafio002/script.js
@@ -5,12 +5,15 @@ function carregar(){
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 6 && hora < 12) {
+        msg.innerHTML = `Bom dia, agora são ${hora} horas`
         img.src = 'imagem/pexels-james-wheeler-1486974.jpg'
         document.body.style.background = '#FF92AF'
     } else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML = `Boa tarde, agora são ${hora} horas`
         img.src = 'imagem/tarde.jpg'
         document.body.style.background = '#FFA521'
     } else {
+        msg.innerHTML = `Boa noite, agora são ${hora} horas`
         img.src = 'imagem/noite.jpg'
         document.body.style.background = '#332A42'
     }
